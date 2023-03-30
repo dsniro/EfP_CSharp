abffc07ba1bf281ee9f5e84f5deba431db2183f8
dsniro <derek.niro@gmail.com>
2023-03-29 20:16:00 -0500

    First Commit

---
+++ PrintingQuotes/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please enter your quote: ");
+String quote = Console.ReadLine();
+
+Console.WriteLine("Who said it? ");
+String person = Console.ReadLine();
+
+Console.WriteLine("{0} said \"{1}\"", person, quote);
+
+// alternative version:
+// Console.WriteLine($"{person} said \"{quote}\"");
