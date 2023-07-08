abb31f788de95d5770e98d1e33abf4e83fbe8184
dsniro <derek.niro@gmail.com>
2023-07-07 19:40:05 -0500

    Add MadLib project

---
+++ MadLib/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter a noun: ");
+String noun = Console.ReadLine();
+System.Console.WriteLine("Enter a verb: ");
+String verb = Console.ReadLine();
+System.Console.WriteLine("Enter an adjective: ");
+String adjective = Console.ReadLine();
+System.Console.WriteLine("Enter an adverb: ");
+String adverb = Console.ReadLine();
+
+// Output
+// Constraints: Use a single output statement.
+System.Console.WriteLine("Do you {0} your {1} {2} {3}? That's hilarious!", verb, adjective, noun, adverb);
