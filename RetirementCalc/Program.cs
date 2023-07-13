3a982d7faea13b6dc1e6584b74d5b74445378604
dsniro <derek.niro@gmail.com>
2023-07-12 20:00:13 -0500

    Add RetirementCalc project

---
+++ RetirementCalc/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("What is your current age?");
+string currAge = Console.ReadLine();
+int currAgeInt = Int32.Parse(currAge);
+Console.WriteLine("What age would you like to retire?");
+string retire = Console.ReadLine();
+int retireInt = Int32.Parse(retire);
+int diff = retireInt - currAgeInt;
+
+Console.WriteLine("You have {0} years left until you can retire.", diff);
+
+int currYear = DateTime.Now.Year;
+int retYear = currYear + diff;
+
+Console.WriteLine("It is {0}, so you can retire in {1}", currYear, retYear);
